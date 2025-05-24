3ee349424fb79cc2154f4f1347571c61a6930ade
Amapola <htrsanbl@gmail.com>
2025-05-24 06:51:23 -0500

    fix: Resolve problems with the last changes capabilities

--- ProjectDfa/Program.cs
+++ ProjectDfa/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjectDfa.RequestObjects;
-using ProjectDfa.Views;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,14 +7,8 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-//If 'permanent' parameter is set the response code is 301, else is 302
-app.MapGet("/", () => Results.Redirect("/home", permanent: true));
-
-//For Frontend Endpoints
-app.MapGet("/home", () => Results.Content(IndexView.Build(), "text/html; charset=utf-8"));
-
-app.MapGet("/regex", () => Results.Content(RegexView.Build(), "text/html; charset=utf-8"));
-
+app.UseDefaultFiles();     // finds index.html automatically
+app.UseStaticFiles();   
 
 //For backend endpoints
 app.MapPost("/validate", ([FromBody] RegexRequest request) => Results.Ok("ok"));
