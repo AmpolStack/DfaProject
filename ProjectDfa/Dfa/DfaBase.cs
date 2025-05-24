898ff3864e9a1cb6238ec83e6a3419a9a9210029
Amapola <htrsanbl@gmail.com>
2025-05-24 17:14:21 -0500

    feat: Was includes Dfa definitions

---
+++ ProjectDfa/Dfa/DfaBase.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectDfa.Dfa;
+
+public class DfaBase<T> where T : Enum
+{
+    public required T StartState { get; set; }
+    public HashSet<T> AcceptStates { get; set; } = [];
+    public Dictionary<(T, char), T> Transitions { get; set; } = new();
+    
+}
---
+++ ProjectDfa/Dfa/IDfa.cs
@@ -0,0 +1,6 @@
+﻿namespace ProjectDfa.Dfa;
+
+public interface IDfa<in TData, TStates> where TStates : Enum
+{
+    public DfaDefinition<TStates> BuildDfa(TData data);
+}
