41ba48212bedc135a89d67331a4163c0be4a95de
Amapola <htrsanbl@gmail.com>
2025-05-24 06:08:31 -0500

    feat: Includes new view

---
+++ ProjectDfa/RequestObjects/RegexRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectDfa.RequestObjects;
+
+public class RegexRequest
+{
+    public required string Input { get; set; }
+    public bool AllowNumbers { get; set; }
+    public bool AllowLetters { get; set; }
+    public bool AllowSpecials { get; set; }
+}
---
+++ ProjectDfa/Views/RegexView.cs
@@ -0,0 +1,6 @@
+﻿namespace ProjectDfa.Views;
+
+public class RegexView
+{
+    
+}
